a472c8e75886ac35f4171a41a4fc622f21ab5620
srrmtt <100603487+srrmtt@users.noreply.github.com>
2022-10-19 11:35:18 +0200

    TODO: weights and connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,78 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
 use libm::exp;
-pub struct Neuron {
+
+pub struct NeuralLayer{
+    neurons: Vec<Neuron>
+}
+
+impl NeuralLayer {
+    pub fn new() -> Self{
+        NeuralLayer { neurons: vec![] }
+    }
+
+    pub fn add_neurons(&mut self, new_neurons: &mut Vec<Neuron>){
+        self.neurons.append(new_neurons);
+    }
+
+    pub fn add_neuron(&mut self, neuron: Neuron){
+        self.neurons.push(neuron);
+    }
+}
+pub struct NeuralNetwork{
+    // neural network parameters
     v_threshold : f32,
     v_rest : f32, 
     v_reset : f32,
+    // LIR model function signature, maybe to be generalized
+    model : Box<dyn Fn( i8, i8, f32, f32, f32, Vec<i8>) -> f32>,
 
-    model : Box<dyn Fn( i8, i8, f32, f32, f32, Vec<i8>) -> f32>
+    input_reader : InputReader,
+
+    layers : Vec<NeuralLayer>,
+
+
+}
+
+impl NeuralNetwork {
+    pub fn new(v_threshold : f32, v_rest : f32, v_reset : f32, model : fn( i8, i8, f32, f32, f32, Vec<i8>) -> f32, npl : &[i8]) -> Self{
+        let mut layers = vec![];
+        
+        for n_neurons in npl {
+            let mut nl = NeuralLayer::new();
+            for _ in 0..*n_neurons{
+                nl.add_neuron(Neuron::new(v_threshold, v_rest, v_reset, model))
+            }
+            layers.push(nl);
+        }
+
+        Self { 
+            v_threshold,
+            v_rest,
+            v_reset, 
+            model: Box::new(model), 
+            input_reader: InputReader::empty_reader(), 
+            layers }
+    }
+
+    pub fn run(&mut self, input_files: &[&str], output_file: &str) -> Result<File, std::io::Error>{
+        self.input_reader = InputReader::from_files(input_files);
+        File::create(output_file)
+    }
+
+}
+
+pub struct Neuron {
+    // neural network parameters
+    v_threshold : f32,
+    v_rest : f32, 
+    v_reset : f32,
+    // LIR model function signature, maybe to be generalized
+    model : Box<dyn Fn( i8, i8, f32, f32, f32, Vec<i8>) -> f32>,
+    // last 'neuron fired' tension 
+    v_mem_old : f32,
+    // last unit time at which the neuron fired
+    ts_1 : i8,
 }
 
 impl Neuron {
@@ -15,9 +81,13 @@ impl Neuron {
             v_threshold, 
             v_rest, 
             v_reset, 
+            v_mem_old: v_rest,
+            ts_1: 0, 
             model: Box::new(model) 
         }
     }
+
+    
 }
 
 
@@ -26,6 +96,9 @@ pub struct InputReader {
 }
 
 impl InputReader {
+    pub fn empty_reader() -> Self {
+        Self { inputs: vec![] }
+    }
     pub fn read_file(filename: &str) -> Result<Vec<i8>, std::io::Error>{
         let file = File::open(filename)?;
         let mut buf_reader = BufReader::new(file);
@@ -53,7 +126,7 @@ impl InputReader {
 
         Self { inputs }
     }
-    pub fn read(&self){
+    pub fn print(&self){
         for i in &self.inputs{
             println!("{:?}", i);
         }
@@ -61,7 +134,7 @@ impl InputReader {
 }
 
 fn main() {
-    println!("--- testing input --- ");
+    
     let model = |ts: i8, ts_1: i8, v_rest: f32, v_mem_old: f32, tao: f64, weights: Vec<i8>| -> f32{
         let k = - (ts - ts_1) as f64 / tao;
         
@@ -75,6 +148,7 @@ fn main() {
     };
     let files = [".\\data\\input1.txt", ".\\data\\input2.txt", ".\\data\\input3.txt"];
     let ir = InputReader::from_files(&files );
-    ir.read()
+    println!("--- testing input --- ");
+    ir.print()
     
 }
